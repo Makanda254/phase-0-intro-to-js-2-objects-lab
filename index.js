9456fa3dfa64081b5b6037a5bd4a3abd602275a0
Victor John Makanda <victormakanda254@gmail.com>
2023-09-14 23:41:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+const employee = {
+    name:"Victor",
+    streetAddress:"Karen road"
+}
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    const newObj = { ...obj };
+
+    newObj[key] = value;
+  
+    return newObj;
+
+}
+
+const newestObj= updateEmployeeWithKeyAndValue(employee,"name", "Sam");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+
+  return obj;
+}
+
+const otherObj= destructivelyUpdateEmployeeWithKeyAndValue(employee,"streetAddress", "11 Broadway");
+
+function destructivelyDeleteFromEmployeeByKey(){
+    delete employee.name;
+    
+    return employee;
+    
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    const oldestObj= { ...employee };
+    delete oldestObj.name;
+    //oldestObj[key] = value;
+    return oldestObj;
+}
+
+
